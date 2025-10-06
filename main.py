01efaa3035ba33b33153b27a53e916692d7b4f45
Hayden2572 <nikitaborisovv353@gmail.com>
2025-10-06 16:18:09 +0300

    sortList

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 listComprehension = [x**2 for x in range(1, 11)]
 listComprehensionSort = [x for x in range(1, 21) if x % 2 == 0]
+ogList = ["python", "Java", "c++", "Rust", "go"]
+sortedList = [word for word in ogList if len(word) > 3 and word.istitle()]
